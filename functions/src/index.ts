ce9b770b9eef877fc8df3c10e999d2c011ae1f32
Phil Gresham <phil@gresham.dev>
2022-08-12 13:34:47 -0700

    Remove unnecessary functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import { CRON_FREQUENCY } from './constants';
-import { Event } from './models';
 import * as AtomFeed from './AtomFeed';
 
 // Fetch the service account key JSON file contents
@@ -17,28 +16,8 @@ let options: admin.AppOptions = {
 if (process.env.env === 'LOCAL') {
   options = functions.config().firebase;
 }
-const app = admin.initializeApp(options);
-functions.logger.log('App initialized', options, app);
-
-export const updateEvent = functions.https.onRequest(async (_, resp) => {
-  const event = new Event('08a0a942-ff4d-4790-99a5-151071c2ac36');
-  event.alertIDs = ['abcd-5435'];
-  try {
-    event.updateAlerts().then((res) => resp.send(res));
-  } catch (e) {
-    resp.status(500).send(e);
-  }
-});
-
-export const fetchAndParseLatestEvents = functions.https.onRequest((_, resp) => {
-  AtomFeed.fetchAndParseLatestEvents()
-    .then((res) => {
-      resp.send(res);
-    })
-    .catch((err) => {
-      resp.status(err?.status ?? 500).send(err);
-    });
-});
+admin.initializeApp(options);
+functions.logger.log('App initialized');
 
 export const scheduledFetchAndParseLatestEvents = functions.pubsub.schedule(CRON_FREQUENCY).onRun((context) => {
   functions.logger.log(`scheduledFetchAndParseLatestEvents runs ${CRON_FREQUENCY}`, context);
