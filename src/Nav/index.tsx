b0e373c95875ffcd24a34c7dfeaba061683aa86b
Phil Gresham <phil@gresham.dev>
2022-08-23 19:49:06 -0700

    Add signin path/components

--- src/Nav/index.tsx
+++ src/Nav/index.tsx
@@ -1,13 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+import { getAuth } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { NavPath } from '../constants';
 
-type NavProps = {};
+const StyledSignoutButton = styled.button`
+  cursor: pointer;
+  color: blue;
+  text-decoration: underline;
+  border: none;
+`;
 
-const Nav = (props: NavProps) => {
+/** `Nav` is the main Navbar of the app. */
+const Nav = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [user] = useAuthState(getAuth());
+
+  const handleSignOut = async () => {
+    await getAuth()
+      .signOut()
+      .then(() => {
+        navigate('/');
+      });
+  };
+  if (location.pathname === NavPath.SignIn) return null;
+
   return (
     <nav>
       <Link to="/">Home</Link>
-      <Link to="/events">Events</Link>
+      <Link to={NavPath.Events}>Events</Link>
+      {user ? (
+        <>
+          <Link to={NavPath.Profile}>Profile</Link>
+          <StyledSignoutButton onClick={handleSignOut}>Sign Out</StyledSignoutButton>
+        </>
+      ) : (
+        <Link to={NavPath.SignIn}>Sign In</Link>
+      )}
     </nav>
   );
 };
