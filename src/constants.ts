df50d7f3e69d6e426c94c8099e75e3e550a07d69
Phil Gresham <phil@gresham.dev>
2022-08-23 19:48:46 -0700

    Add route constants

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export namespace NavPath {
+  export const Events = '/events';
+  export const Landing = '/landing';
+  export const PrivacyPolicy = '/privacy';
+  export const Profile = '/profile';
+  export const SignIn = '/signin';
+  export const ToS = '/tos';
+}
