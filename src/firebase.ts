24d2211ca9f870621c34a7a7e4ca007064deb482
Phil Gresham <phil@gresham.dev>
2022-09-07 09:23:29 -0700

    Revert "Attempt test setup with Firebase"
    This reverts commit a0f3c35fc1446ebd939c104aa970be433b3a0fd5.

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getPerformance } from 'firebase/performance';
 
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,29 +1,5 @@
-import 'dotenv';
-import fs from 'fs';
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import { initializeApp } from 'firebase/app';
-import { initializeTestEnvironment } from '@firebase/rules-unit-testing';
-import type { RulesTestEnvironment } from '@firebase/rules-unit-testing';
-
-const databaseRules = JSON.parse(fs.readFileSync(__dirname + '/../database.rules.json', 'utf-8'));
-const demoProjectID = `demo-${process.env.REACT_APP_FIREBASE_PROJECT_ID}`;
-
-/** `initializeFirebaseTestEnv` is a test-only util to be used within `beforeAll` and the like */
-export const initializeFirebaseTestEnv = async () => {
-  const testEnv = await initializeTestEnvironment({
-    projectId: demoProjectID,
-    database: {
-      rules: databaseRules,
-      port: 9000,
-      host: '127.0.0.1',
-    },
-  });
-  initializeApp({
-    projectId: demoProjectID,
-  });
-  return testEnv;
-};
-
-beforeAll(initializeFirebaseTestEnv);
-
-export type { RulesTestEnvironment };
