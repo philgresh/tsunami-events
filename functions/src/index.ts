995e723d4c375613050f8f9b70264d8e145ce9c3
Phil Gresham <phil@gresham.dev>
2022-09-19 20:29:12 -0700

    Nix ExpressJS-based `api`
    Was causing issues

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,5 @@
 import './firebase'; // Keep as top, initializes Firebase app
 
-export * from './modules/api';
 export * from './modules/callables';
 export * from './modules/listeners';
 export * from './modules/scheduled';
