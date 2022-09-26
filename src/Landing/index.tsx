32dd379124608a04af008bfbeff9e33ca10a7d02
Phil Gresham <phil@gresham.dev>
2022-09-26 13:09:04 -0700

    Minor update `Landing`

--- src/Landing/index.tsx
+++ src/Landing/index.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
-import { Stack, Typography, useTheme } from '@mui/material';
+import { Box, Container, Stack, Typography, useTheme } from '@mui/material';
 import { renderToString } from 'react-dom/server';
 
-const HEADLINE = 'Be the first to know about a potential tsunami event.';
+const HEADLINE = 'A tsunami can strike any ocean coast at any time.';
+const SUBHEADLINE = 'Stay informed about these potentially deadly events as they happen.';
 
 const Headline = () => {
   const theme = useTheme();
   return (
-    <Typography variant="h4" sx={{ fontFamily: 'Poppins' }} color={theme.palette.text.primary}>
-      {HEADLINE}
-    </Typography>
+    <div>
+      <Typography variant="h4" component="h1" sx={{ fontFamily: 'Poppins' }} color={theme.palette.text.primary}>
+        {HEADLINE}
+      </Typography>
+      <Typography variant="h6" component="h2" color={theme.palette.text.secondary}>
+        {SUBHEADLINE}
+      </Typography>
+    </div>
   );
 };
 
 const Landing = () => {
+  const theme = useTheme();
+
   return (
-    <Stack spacing={2}>
-      <Headline />
-    </Stack>
+    <Container>
+      <Stack spacing={2}>
+        <Headline />
+      </Stack>
+    </Container>
   );
 };
 
--- src/Landing/stories/index.stories.tsx
+++ src/Landing/stories/index.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Box, useTheme } from '@mui/material';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Landing from '..';
 import { ThemeWrapper } from '../../../.storybook/preview';
@@ -9,9 +10,29 @@ export default {
   argTypes: {},
 } as ComponentMeta<typeof Landing>;
 
+const Background = (props: any) => {
+  const theme = useTheme();
+  return (
+    <Box
+      style={{
+        backgroundColor: theme.palette.background.paper,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      {props.children}
+    </Box>
+  );
+};
+
 const Template: ComponentStory<typeof Landing> = () => (
   <ThemeWrapper>
-    <Landing />
+    <Background>
+      <Landing />
+    </Background>
   </ThemeWrapper>
 );
 
