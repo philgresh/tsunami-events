bcfb622607ebc55f3319c9059f157b99b6040769
Phil Gresham <phil@gresham.dev>
2022-10-13 22:58:01 -0700

    Remove `Events` nav item

--- src/Nav/constants.ts
+++ src/Nav/constants.ts
@@ -1,11 +1,4 @@
-import { NavPath } from '../constants';
 import type { NavItem } from './types';
 
 export const DRAWER_WIDTH = 240;
-export const NAV_ITEMS: NavItem[] = [
-  {
-    title: 'Events',
-    path: NavPath.Events,
-    matchingPaths: new Set([NavPath.EventsCollection, NavPath.Events]),
-  },
-];
+export const NAV_ITEMS: NavItem[] = [];
