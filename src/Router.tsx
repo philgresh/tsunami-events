da16f68c916e6b31aeb958f7dc272f333b19464c
Phil Gresham <phil@gresham.dev>
2022-10-13 22:58:21 -0700

    Improve containerization of routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,13 +4,22 @@ import Container from '@mui/material/Container';
 import { getAuth } from 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { BrowserRouter, Navigate, Outlet, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import { NavPath, NAVBAR_HEIGHT } from './constants';
 import Landing from './Landing';
 import Navbar, { Loading } from './Nav';
+import type { Breakpoint } from '@mui/material';
 
 const Signin = lazy(() => import('./auth/Signin'));
 const Account = lazy(() => import('./Account'));
 const SMSToS = lazy(() => import('./components/SMSToS'));
+
+const StyledMain = styled.main`
+  position: absolute;
+  top: ${NAVBAR_HEIGHT};
+  left: 0;
+  width: 100%;
+`;
 
 const ProtectedRoute = memo(
   ({
@@ -33,48 +42,64 @@ const ProtectedRoute = memo(
 
 const Router = () => {
   const [user] = useAuthState(getAuth());
+
+  const ContainerizedRoute = ({ maxWidth, children }: { maxWidth?: Breakpoint; children?: React.ReactNode }) => (
+    <Container maxWidth={maxWidth} sx={{ position: 'absolute', top: NAVBAR_HEIGHT }} component="main">
+      {children}
+    </Container>
+  );
+
   return (
     <BrowserRouter>
       <Navbar />
-      <Container maxWidth="md" sx={{ position: 'fixed', top: NAVBAR_HEIGHT }} component="main">
-        <Routes>
-          <Route path={NavPath.Landing} element={<Landing />} />
-          <Route
-            path={NavPath.Account}
-            element={
-              <ProtectedRoute isAllowed={!!user} redirectPath={NavPath.SignIn}>
-                <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route
+          path={NavPath.Landing}
+          element={
+            <StyledMain>
+              <Landing />
+            </StyledMain>
+          }
+        />
+        <Route
+          path={NavPath.Account}
+          element={
+            <ProtectedRoute isAllowed={!!user} redirectPath={NavPath.SignIn}>
+              <Suspense fallback={<Loading />}>
+                <ContainerizedRoute maxWidth="md">
                   <Account user={user!} />
-                </Suspense>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path={NavPath.SignIn}
-            element={
-              <Suspense fallback={<Loading />}>
-                <Signin />
+                </ContainerizedRoute>
               </Suspense>
-            }
-          />
-          <Route
-            path={NavPath.SMSToS}
-            element={
-              <Suspense fallback={<Loading />}>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path={NavPath.SignIn}
+          element={
+            <Suspense fallback={<Loading />}>
+              <Signin />
+            </Suspense>
+          }
+        />
+        <Route
+          path={NavPath.SMSToS}
+          element={
+            <Suspense fallback={<Loading />}>
+              <ContainerizedRoute>
                 <SMSToS />
-              </Suspense>
-            }
-          />
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: '1rem' }}>
-                <p>There's nothing here!</p>
-              </main>
-            }
-          />
-        </Routes>
-      </Container>
+              </ContainerizedRoute>
+            </Suspense>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <ContainerizedRoute>
+              <p>There's nothing here!</p>
+            </ContainerizedRoute>
+          }
+        />
+      </Routes>
     </BrowserRouter>
   );
 };
