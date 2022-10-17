eb4efa97de9232b2484d161a9d8943cca54b69f4
Phil Gresham <phil@gresham.dev>
2022-10-17 15:24:55 -0700

    Add `Footer` to `Landing`

--- src/Landing/index.tsx
+++ src/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from './Footer';
 import Hero from './Hero';
 import HowItWorks from './HowItWorks';
 
@@ -7,6 +8,7 @@ const Landing = () => {
     <>
       <Hero />
       <HowItWorks />
+      <Footer />
     </>
   );
 };
