f46a35ffc5c451de8254e00e5575596f2f5e00ca
Phil Gresham <phil@gresham.dev>
2022-10-17 19:05:19 -0700

    Move stories to be closer to source

--- src/components/TOS.stories.tsx
+++ src/components/TOS.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import ToSComponent from '../../components/ToS';
+import ToSComponent from './ToS';
 
 export default {
   title: 'TOS/Terms of Service',
